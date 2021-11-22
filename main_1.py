9808e81be950b02fef8d14edca853426f9deff68
alina <Alina.Shakova.45@yandex.ru>
2021-11-22 14:37:03 +0500

    отображает информацию о кофе

---
+++ main_1.py
@@ -0,0 +1,25 @@
+import sys
+import sqlite3
+from PyQt5 import uic
+from PyQt5.QtWidgets import QApplication, QMainWindow
+
+
+class Coffee(QMainWindow):
+    def __init__(self):
+        super().__init__()
+        inf = ''
+        uic.loadUi('main.ui', self)
+        con = sqlite3.connect("coffee.db")
+        cur = con.cursor()
+        info = cur.execute("SELECT * FROM coffee")
+        for i in info:
+            inf += str(list(i)[1:-1])
+        con.close()
+        self.label.setText(inf)
+
+
+if __name__ == '__main__':
+    app = QApplication(sys.argv)
+    window = Coffee()
+    window.show()
+    app.exec_()
