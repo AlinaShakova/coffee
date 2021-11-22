640074baa5d5bb05437d6a20619c62e861dc927b
alina <Alina.Shakova.45@yandex.ru>
2021-11-22 17:33:45 +0500

    осуществляет добавление и редактирование данных

--- main_2.py
+++ main_2.py
@@ -8,13 +8,22 @@ class Coffee(QMainWindow):
     def __init__(self):
         super().__init__()
         uic.loadUi('addEditCoffeeForm.ui', self)
+        self.pushButton.clicked.connect(self.chang)
+
+    def chang(self):
         con = sqlite3.connect("coffee.db")
         cur = con.cursor()
-        if self.textEdit.setText() != '':
-            a = self.textEdit.setText()
-        #cor.execute("""UPDATE films SET duration = 42 WHERE NOT duration""")
-
-        cur.execute(f"""INSERT INTO coffee('variety, roasting, condition, taste, price, volume') VALUES({a})""")
+        if self.lineEdit.text() != '':
+            a = self.lineEdit.text().split()
+            if len(a) == 6:
+                a = f"'{a[0]}', '{a[1]}', '{a[2]}', '{a[3]}', '{a[4]}', '{a[5]}'"
+                cur.execute(f"""INSERT INTO coffee(variety, roasting, condition, taste, price, volume)
+                                VALUES({a})""")
+        if self.lineEdit1.text() != '' and self.lineEdit2.text() != '':
+            a, b = self.lineEdit1.text().split(), self.lineEdit2.text()
+            if len(a) == 2:
+                a1, a2 = a[0], a[1]
+                cur.execute(f"""UPDATE coffee SET {a1} = {b} WHERE id = {a2}""")
         con.commit()
         con.close()
 
