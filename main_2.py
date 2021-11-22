37bc5e79aa1070901705e4bf3440be8969a757fc
alina <Alina.Shakova.45@yandex.ru>
2021-11-22 15:01:50 +0500

    отображает информацию о кофе

--- main_2.py
+++ main_2.py
@@ -7,16 +7,16 @@ from PyQt5.QtWidgets import QApplication, QMainWindow
 class Coffee(QMainWindow):
     def __init__(self):
         super().__init__()
-        inf = ''
-        uic.loadUi('main.ui', self)
+        uic.loadUi('addEditCoffeeForm.ui', self)
         con = sqlite3.connect("coffee.db")
         cur = con.cursor()
-        info = cur.execute("SELECT * FROM coffee")
-        for i in info:
-            inf += str(list(i)[1:-1])
-        print(inf)
+        if self.textEdit.setText() != '':
+            a = self.textEdit.setText()
+        #cor.execute("""UPDATE films SET duration = 42 WHERE NOT duration""")
+
+        cur.execute(f"""INSERT INTO coffee('variety, roasting, condition, taste, price, volume') VALUES({a})""")
+        con.commit()
         con.close()
-        self.label.setText(inf)
 
 
 if __name__ == '__main__':
