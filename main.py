f21b5a61bb0f9f8bd7a6061ce408faf0e75513dd
alina <Alina.Shakova.45@yandex.ru>
2021-11-22 14:34:28 +0500

    интерфейс добавления и редактирования

--- main.py
+++ main.py
@@ -18,6 +18,23 @@ class Coffee(QMainWindow):
         self.label.setText(inf)
 
 
+class Test:
+    def __init__(self):
+        con = sqlite3.connect("exam.db")
+        cur = con.cursor()
+
+        block_columns = [f"'{i}'" for i in self.count_questions]
+        block_names = [f"{self.count_questions[i]}" for i in self.count_questions]
+
+        names_columns = f"name, {', '.join(block_columns)}, flag_time, time, last_number"
+        value_columns = f"'{self.name}', {', '.join(block_names)}, {self.flag_time}, '{self.time}', 1"
+        cor.execute("""UPDATE films SET duration = 42 WHERE NOT duration""")
+
+        cur.execute(f"""INSERT INTO Tests({names_columns}) VALUES({value_columns})""")
+        con.commit()
+        con.close()
+
+
 if __name__ == '__main__':
     app = QApplication(sys.argv)
     window = Coffee()
