7b1c713b7798f4b8b610db4fc88f376ef4d3d67f
alina <Alina.Shakova.45@yandex.ru>
2021-11-25 16:35:49 +0500

    отображает инормацию о кофе

--- main_2.py
+++ main_2.py
@@ -17,13 +17,13 @@ class Coffee(QMainWindow):
             a = self.lineEdit.text().split()
             if len(a) == 6:
                 a = f"'{a[0]}', '{a[1]}', '{a[2]}', '{a[3]}', '{a[4]}', '{a[5]}'"
-                cur.execute(f"""INSERT INTO coffee(variety, roasting, condition, taste, price, volume)
+                cur.execute(f"""INSERT INTO cofe(variety, roasting, condition, taste, price, volume)
                                 VALUES({a})""")
         if self.lineEdit1.text() != '' and self.lineEdit2.text() != '':
             a, b = self.lineEdit1.text().split(), self.lineEdit2.text()
             if len(a) == 2:
                 a1, a2 = a[0], a[1]
-                cur.execute(f"""UPDATE coffee SET {a1} = {b} WHERE id = {a2}""")
+                cur.execute(f"""UPDATE cofe SET {a1} = {b} WHERE id = {a2}""")
         con.commit()
         con.close()
 
